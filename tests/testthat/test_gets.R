bcd84bc9ac9df8f8111d1f02e532519bd996055f
ironholds <ironholds@gmail.com>
2015-02-03 10:05:00 -0500

    add unit tests

---
+++ tests/testthat/test_gets.R
@@ -0,0 +1,30 @@
+context("Get functions")
+
+test_that("A specific item can be retrieved with an entire item code", {
+  expect_true({get_item("Q100");TRUE})
+})
+
+test_that("A specific item can be retrieved with a partial entire item code", {
+  expect_true({get_item("100");TRUE})
+})
+
+test_that("A specific property can be retrieved with an entire prop code + namespace", {
+  expect_true({get_property("Property:P10");TRUE})
+})
+
+test_that("A specific property can be retrieved with an entire prop code + namespace", {
+  expect_true({get_property("P10");TRUE})
+})
+
+
+test_that("A specific property can be retrieved with a partial prop code", {
+  expect_true({get_property("10");TRUE})
+})
+
+test_that("A randomly-selected item can be retrieved",{
+  expect_true({get_random_item();TRUE})
+})
+
+test_that("A randomly-selected property can be retriveed",{
+  expect_true({get_random_property();TRUE})
+})
---
+++ tests/testthat/test_search.R
@@ -0,0 +1,17 @@
+context("Get functions")
+
+test_that("English-language search works",{
+  expect_true({find_item("Wonder Girls", "en");TRUE})
+})
+
+test_that("Non-English-language search works",{
+  expect_true({find_item("Wonder Girls", "es");TRUE})
+})
+
+test_that("Search with limit modding works",{
+  expect_that(length(find_item("Wonder Girls", "en", 3)), equals(3))
+})
+
+test_that("Property search works",{
+  expect_true({find_property("Music", "en");TRUE})
+})
